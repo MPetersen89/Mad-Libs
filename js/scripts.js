871ca5d1ff9cd14126c72e8a684465344bbb5337
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-08 14:46:16 -0700

    refactored mad-libs.js to utilize array and loops for inputs.
    Co-authored by: Janet Karpenske <janetkarpenske@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    // const person1Input = $("input#person1").val();
+    // const person2Input = $("input#person2").val();
+    // const animalInput = $("input#animal").val();
+    // const exclamationInput = $("input#exclamation").val();
+    // const verbInput = $("input#verb").val();
+    // const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    // const inputArray = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
+    const spanArray = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $("#story").show();
+    spanArray.forEach(function(span) {
+      let input = $("input#" + span).val();
+      $("." + span).text(input);
+    });
+
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
+
+    // $("#story").show();
+
+    $("#storyArray").show();
 
     event.preventDefault();
   });
